a448fe083ba34ce6edd3f83780732e0c23fd7cd1
Frederik Claus <f.v.claus@googlemail.com>
2019-09-21 08:53:30 +0200

    Removes property picker when only one property is left

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,8 @@ import Window = vscode.window;
 import QuickPickItem = vscode.QuickPickItem;
 import TextEditor = vscode.TextEditor;
 
-function intersection<E> (setA : Set<E>, setB : Set<E>) {
-    var _intersection = new Set();
+function intersection<E>(setA: Set<E>, setB: Set<E>): Set<E> {
+    var _intersection = new Set<E>();
     for (var elem of setB) {
         if (setA.has(elem)) {
             _intersection.add(elem);
@@ -16,8 +16,7 @@ function intersection<E> (setA : Set<E>, setB : Set<E>) {
     return _intersection;
 }
 
-
-function sortByProperty (a: any, b : any, property: string) : number {
+function sortByProperty(a: any, b: any, property: string): number {
     const aValue = a[property];
     const bValue = b[property];
     if (aValue > bValue) {
@@ -29,38 +28,54 @@ function sortByProperty (a: any, b : any, property: string) : number {
     }
 }
 
-function pickUntilSortIsDeterministic<E>(window: typeof Window, selectedProperties: string[], quickPickItems: QuickPickItem[], arrayToSort : E[] ) : Promise<E[]>{
-    const remainingQuickPickItems = quickPickItems.filter(item => selectedProperties.indexOf(item.label) === -1);
-    return new Promise((resolve) => {
-        window.showQuickPick(remainingQuickPickItems, {
-            canPickMany: false,
-            placeHolder: selectedProperties.length === 0? 'Pick property from list to define sort order.' : 'Sorting cannot produce determinable result. Please pick another property.'
-        }).then(selectedItem => {
-            if (selectedItem) {
-                const selectedProperty = selectedItem.label;
-                selectedProperties.push(selectedProperty);
-    
-                let sortIsDeterminstic = true;
-                arrayToSort.sort((a, b) => {
-                    for(const selectedProperty of selectedProperties) {
-                        const order = sortByProperty(a, b, selectedProperty);
-                        if (order !== 0) {
-                            return order;
-                        }
-                    }                
-                    sortIsDeterminstic = false;
-                    return 0;
-                });
-    
-                // The sort is deterministic or the array cannot be sorted in a determinable way.
-                if (sortIsDeterminstic || selectedProperties.length === quickPickItems.length) {
-                   resolve(arrayToSort);
-                } else {    
-                    pickUntilSortIsDeterministic(window, selectedProperties, quickPickItems, arrayToSort)
-                        .then(arrayToSort => resolve(arrayToSort));
-                }
+function sortByProperties(arrayToSort: any[], selectedProperties: string[]): boolean {
+    let sortIsDeterminstic = true;
+    arrayToSort.sort((a, b) => {
+        for (const selectedProperty of selectedProperties) {
+            const order = sortByProperty(a, b, selectedProperty);
+            if (order !== 0) {
+                return order;
             }
+        }
+        sortIsDeterminstic = false;
+        return 0;
+    });
+
+    return sortIsDeterminstic;
+}
+
+function pickPropertiesIfNecessary(window: typeof Window, selectedProperties: string[], quickPickItems: QuickPickItem[]): Thenable<vscode.QuickPickItem | undefined> {
+    const remainingQuickPickItems = quickPickItems.filter(item => selectedProperties.indexOf(item.label) === -1);
+    // Don't show picker if only one item is remaining.
+    if (remainingQuickPickItems.length > 1) {
+        return window.showQuickPick(remainingQuickPickItems, {
+            canPickMany: false,
+            placeHolder: selectedProperties.length === 0 ? 'Pick property from list to define sort order.' : 'Sorting cannot produce determinable result. Please pick another property.'
         });
+    } else {
+        return Promise.resolve(remainingQuickPickItems[0]);
+    }
+}
+
+function pickUntilSortIsDeterministic<E>(window: typeof Window, selectedProperties: string[], quickPickItems: QuickPickItem[], arrayToSort: E[]): Promise<E[]> {
+    return new Promise((resolve) => {
+        pickPropertiesIfNecessary(window, selectedProperties, quickPickItems)
+            .then(selectedItem => {
+                if (selectedItem) {
+                    const selectedProperty = selectedItem.label;
+                    selectedProperties.push(selectedProperty);
+
+                    let sortIsDeterminstic = sortByProperties(arrayToSort, selectedProperties);
+
+                    // The sort is deterministic or the array cannot be sorted in a determinable way.
+                    if (sortIsDeterminstic || selectedProperties.length === quickPickItems.length) {
+                        resolve(arrayToSort);
+                    } else {
+                        pickUntilSortIsDeterministic(window, selectedProperties, quickPickItems, arrayToSort)
+                            .then(arrayToSort => resolve(arrayToSort));
+                    }
+                }
+            });
     });
 
 }
@@ -68,10 +83,6 @@ function pickUntilSortIsDeterministic<E>(window: typeof Window, selectedProperti
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sort-json-array" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -91,7 +102,7 @@ export function activate(context: vscode.ExtensionContext) {
                 if (parsedJson.constructor === Array) {
                     const parsedArray = (parsedJson as any[]);
                     const commonProperties = parsedArray
-                        .map(o  => new Set(Object.getOwnPropertyNames(o)))
+                        .map(o => new Set(Object.getOwnPropertyNames(o)))
                         // Intersection is associative
                         .reduce((a, b) => intersection(a, b));
                     const quickPickItems = Array.from(commonProperties)
@@ -102,11 +113,11 @@ export function activate(context: vscode.ExtensionContext) {
                         window.showErrorMessage(`There are no properties all objects of this array have in common.`);
                     } else {
                         pickUntilSortIsDeterministic(window, [], quickPickItems, parsedArray.slice(0))
-                        .then(sortedArray => {
-                            editor.edit(edit => {
-                                edit.replace(selection, JSON.stringify(sortedArray, null, editor.options.tabSize));
+                            .then(sortedArray => {
+                                editor.edit(edit => {
+                                    edit.replace(selection, JSON.stringify(sortedArray, null, editor.options.tabSize));
+                                });
                             });
-                        });
                     }
                 } else {
                     window.showErrorMessage(`Selection is a ${parsedJson.constructor} not an array.`);
