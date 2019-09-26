2dc9b0e2e2b32f7651aefa8b7f9d6af243cd54d3
Frederik Claus <f.v.claus@googlemail.com>
2019-09-26 20:00:54 +0200

    Adds tests for determineArrayType

--- src/determineArrayType.ts
+++ src/determineArrayType.ts
@@ -4,27 +4,19 @@ export enum ArrayType {
     number
 }
 function determineType(item: any) {
+    if (item === null || item === undefined) {
+        return undefined;
+    }
     const arrayTypeString = typeof item as keyof typeof ArrayType;
 
     return ArrayType[arrayTypeString];
 
 }
-export function determineArrayType(array: any[]): ArrayType | undefined {
-    let arrayType: ArrayType | undefined;
-    let i = 0;
-    do {
-        const item = array[i];
-        const itemType = determineType(item);
-        // null or undefined would need to be handled by every sort function. This is why we don't allow it here.
-        const typeOfCurrentItemIsDifferent = itemType !== arrayType;
-        const itemIsNullOrUndefined = (item === null || item === undefined);
-        if (itemIsNullOrUndefined || typeOfCurrentItemIsDifferent) {
-            return undefined;
-        } else {
-            arrayType = itemType;
-        }
-        i++;
-    } while (arrayType !== undefined && i < array.length)
-    
-    return arrayType;
+export function determineArrayType(array: any[]): ArrayType | undefined { 
+    if (array.length === 0) {
+        return undefined;
+    }
+    return array
+        .map(determineType)
+        .reduce((arrayType, itemType) => arrayType === itemType? arrayType : undefined as any);
 }
--- src/test/suite/determineArrayType.test.ts
+++ src/test/suite/determineArrayType.test.ts
@@ -1,10 +1,36 @@
-import * as assert from 'assert';
+import { determineArrayType, ArrayType } from "../../determineArrayType";
 
+import chai = require('chai');
+const expect = chai.expect;
 
-suite('Extension Test Suite', () => {
+describe('Determine array type', () => {
 
-	test('Sample test', () => {
-		assert()
-		assert.equal([1, 2, 3].indexOf(0), -1);
+	const UNSUPPORTED_ARRAYS = [
+		[],
+		[null],
+		([1, 'foo'] as any[]),
+		([1, null, 'foo'] as any[]),
+		([{'foo': 'bar'}, 1]),
+
+	]
+
+	UNSUPPORTED_ARRAYS.forEach(unsupportedArray => {
+		it(`should not support array ${JSON.stringify(unsupportedArray)}`, () => {
+			expect(determineArrayType(unsupportedArray)).to.be.undefined;
+		});
 	});
+
+	const SUPPORTED_ARRAYS = [
+		[[1], ArrayType.number],
+		[[10, 20, 1], ArrayType.number],
+		[['foo'], ArrayType.string],
+		[['foo', 'bar'], ArrayType.string],
+		[[{'age': 10}, {'age': 20}], ArrayType.object]
+	]
+
+	SUPPORTED_ARRAYS.forEach(([supportedArray, expectedType]) => {
+		it(`should supported supported ${JSON.stringify(supportedArray)}`, () => {
+			expect(determineArrayType(supportedArray as any[])).to.equal(expectedType);
+		})
+	})
 });
