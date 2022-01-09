9d6144dc76fc729be3c486ac9d216ca64cf06cfe
Frederik Claus <f.v.claus@googlemail.com>
2022-01-09 14:58:31 +0100

    Disables workspace-trust during test

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,13 +4,11 @@ import {runTests} from 'vscode-test';
 
 async function main(): Promise<void> {
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-    // The path to the extension test script
-    // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const workspacePath = path.resolve(extensionDevelopmentPath, 'src/test/workspace')
+
+    console.log(`Using workspacePath ${workspacePath}`)
 
     const version = process.env.VSCODE_VERSION != null? process.env.VSCODE_VERSION : '1.44.0';
     // Download VS Code, unzip it and run the integration test
@@ -18,7 +16,9 @@ async function main(): Promise<void> {
     // Use 1.38.0, becaues lower version would not close editor when tests are finished.
     await runTests({version,
       extensionDevelopmentPath,
-      extensionTestsPath});
+      extensionTestsPath,
+      launchArgs: [workspacePath, "--disable-workspace-trust"]
+    });
   } catch (err) {
     console.error('Failed to run tests');
     process.exit(1);
--- src/test/suite/sortOrder/sortNumberOrStrings/sortNumberOrStrings.vsc-test.ts
+++ src/test/suite/sortOrder/sortNumberOrStrings/sortNumberOrStrings.vsc-test.ts
@@ -1,11 +1,30 @@
 import {triggerSortCommandExpectSuccess} from '../../triggerSortCommandExpectSucccess';
 
+import * as vscode from 'vscode';
+
 import {afterEach} from 'mocha';
 import {closeActiveEditor} from '../../textEditorUtils';
+import { ExtensionConfiguration } from '../../../../sortOrder';
+
+async function updateConfiguration(expectedConfiguration: ExtensionConfiguration): Promise<void> {
+  const actualConfiguration = vscode.workspace.getConfiguration("sortJsonArray.collation");
+  await actualConfiguration.update("locale", expectedConfiguration.collation.locale);
+  await actualConfiguration.update("caseFirst", expectedConfiguration.collation.caseFirst);
+  await actualConfiguration.update("ignorePunctuation", expectedConfiguration.collation.ignorePunctuation);
+  await actualConfiguration.update("numeric", expectedConfiguration.collation.numeric);
+}
 
 suite('Sort number or strings', function() {
   afterEach(async () => {
     await closeActiveEditor();
+    await updateConfiguration({
+      collation: {
+        locale: "system",
+        ignorePunctuation: false,
+        numeric: false,
+        caseFirst: "false"
+      }
+    })
   });
 
   test('should sort numbers', async function() {
@@ -15,4 +34,29 @@ suite('Sort number or strings', function() {
   test('should sort strings', async function() {
     await triggerSortCommandExpectSuccess('extension.sortJsonArrayAscending', ['foo', 'bar', 'car'], ['bar', 'car', 'foo']);
   });
+
+  test('should sort strings using Intl.Collator', async function() {
+    await updateConfiguration({
+      collation: {
+        caseFirst: "upper",
+        numeric: true,
+        ignorePunctuation: true,
+        locale: "da"
+      }
+    })
+    await triggerSortCommandExpectSuccess('extension.sortJsonArrayAscending', [
+      "'Bar'",
+      "1",
+      "10",
+      "2",
+      "bar",
+      "bar",
+      "Bar",
+      "foo",
+      "Foo",
+      "ö",
+      "y"
+    ], ["1", "2", "10", "'Bar'", "Bar", "bar", "bar", "Foo", "foo", "y", "ö"]);
+  });
+
 });
